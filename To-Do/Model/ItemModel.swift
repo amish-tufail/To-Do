141e2263d969a34c6f083e8f79edb3f5be9cbe71
Amish <amish.tufail2002@gmail.com>
2024-04-16 21:05:34 +0500

    - List View, Add VIew, ItemModel

---
+++ To-Do/Model/ItemModel.swift
@@ -0,0 +1,8 @@
+//
+//  ItemModel.swift
+//  To-Do
+//
+//  Created by Amish on 16/04/2024.
+//
+
+import Foundation
---
+++ To-Do/View/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  To-Do
+//
+//  Created by Amish on 16/04/2024.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
