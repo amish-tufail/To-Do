008e6b9aa7061cedbc386a70f0848a49e1585924
Amish <amish.tufail2002@gmail.com>
2024-04-16 20:34:14 +0500

    Initial Commit

---
+++ To-Do/To_DoApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_DoApp.swift
+//  To-Do
+//
+//  Created by Amish on 16/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct To_DoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
