3102075289901839afe5f16d24119a1cd8c30568
Amish <amish.tufail2002@gmail.com>
2024-04-16 22:03:46 +0500

    - UserDeaults, saving items

--- To-Do/ViewModel/ListViewModel.swift
+++ To-Do/ViewModel/ListViewModel.swift
@@ -10,18 +10,28 @@ import Observation
 
 @Observable
 class ListViewModel {
-    var items: [ItemModel] = []
+    let itemsKey = "items_list"
+    var items: [ItemModel] = [] { // When changes didSet gets call
+        didSet {
+            saveItems()
+        }
+    }
     init() {
         getItems()
     }
     
     private func getItems() {
-        let newItems = [
-            ItemModel(title: "This is the first title!", isCompleted: false),
-            ItemModel(title: "Second title!", isCompleted: true),
-            ItemModel(title: "Third title!", isCompleted: false)
-         ]
-        items.append(contentsOf: newItems)
+//        let newItems = [
+//            ItemModel(title: "This is the first title!", isCompleted: false),
+//            ItemModel(title: "Second title!", isCompleted: true),
+//            ItemModel(title: "Third title!", isCompleted: false)
+//         ]
+//        items.append(contentsOf: newItems)
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        self.items = savedItems
     }
     
     func onDelete(indexSet: IndexSet) {
@@ -40,6 +50,12 @@ class ListViewModel {
     func updateItem(item: ItemModel) {
         if let index = items.firstIndex(where: { $0.id == item.id}) {
             items[index] = item.updateCompletion() // return ItemModel
+        }
+    }
+    
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
         }
     }
 }
